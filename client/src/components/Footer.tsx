47ab26a92a2d06a8777d311f47d33a7f65e883bd
Mr Groot <113226362+MrGrootx@users.noreply.github.com>
2024-03-26 08:48:15 +0530

    feat: components

---
+++ client/src/components/Footer.tsx
@@ -0,0 +1,17 @@
+const Footer = () => {
+  return (
+    <div className="bg-blue-700 py-6">
+      <div className="container mx-auto flex justify-between items-center">
+            <span className="text-3xl text-white font-bold tracking-tight">
+               grootHolidays.com
+            </span>
+            <span className="text-white font-bold tracking-tight flex gap-4">
+               <p className="cursor-pointer">Privacy Policy</p>
+               <p className="cursor-pointer">Tos</p>
+            </span>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
